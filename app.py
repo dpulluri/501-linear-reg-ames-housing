4f78acfd2bd7a0955dfe409f9098e22d63543bd9
dheerajp316 <dheeraj_pulluri@intuit.com>
2022-04-18 23:42:45 -0700

    update eqn

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ app.layout = html.Div(children=[
     State(component_id='BedroomAbvGr', component_property='value'),
     State(component_id='TotalSF', component_property='value'),
     State(component_id='SingleFam', component_property='value'),
-    State(component_id='LargeNeighborhood', component_property='value')
+    State(component_id='LargeNeighborhood', component_property='value'),
     State(component_id='CentralAirCondition', component_property='value')
 )
 def ames_lr_function(clicks, YearBuilt,Bathrooms,BedroomAbvGr,TotalSF,SingleFam,LargeNeighborhood, CentralAirCondition):
